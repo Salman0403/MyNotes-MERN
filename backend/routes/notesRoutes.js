d78bec70277e8dd9a04a5db19531dfe4f5e6ed8c
Salman0403 <muhammadsalman0403@gmail.com>
2023-03-07 19:39:21 +0500

    feat: login with token

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { getAllNotes } = require("../controllers/notesControllers");
+const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.route('/').get(getAllNotes)
+router.route('/').get(protect,getAllNotes)
 // router.route('/create').post()
 // router.route('/:id').get().put().delete()
 
