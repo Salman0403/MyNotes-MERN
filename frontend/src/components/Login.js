2c2e5c96640e636d2d95ce7fa70e0a9db893f113
Salman0403 <muhammadsalman0403@gmail.com>
2023-03-12 20:56:10 +0500

    feat: simple login form structure

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-    return(
-        <h1>Login Form</h1>
-    )
-}
+  return (
+    <div>
+      <h1>Login Form</h1>
 
-export default Login
+      <input type='email' placeholder="Enter your email"/>
+      <input type='password' placeholder="Enter your password"/>
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default Login;
