e7f5658066da9a538362883645cbb577837312c0
Salman0403 <muhammadsalman0403@gmail.com>
2023-03-15 14:03:24 +0500

    feat: input field component

--- frontend/src/components/InputFields/BasicField.js
+++ frontend/src/components/InputFields/BasicField.js
@@ -1,6 +1,10 @@
-export const BasicField = (props) =>{
-    console.log('BasicField_props: ', props);
-    return(
-        <input/>
-    )
-}
+import { Row, Col, Input } from "antd";
+
+export const BasicField = (props) => {
+  console.log("BasicField_props: ", props);
+  return (
+    <Col span={24}>
+      <Input {...props} />
+    </Col>
+  );
+};
--- frontend/src/container/Login/Login.js
+++ frontend/src/container/Login/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col } from "antd";
+import { BasicField } from "../../components/InputFields/BasicField";
 
 const Login = () => {
   return (
@@ -9,15 +10,15 @@ const Login = () => {
       align="middle"
     >
       <Col style={{ background: "blue" }}>
-        <Row gutter={[10, 10]} style={{ background: "green" }}>
+        <Row gutter={[10, 10]} style={{ background: "green" }} justify="center">
           <Col span={24} style={{ background: "orange" }}>
             <h1>Login Form</h1>
           </Col>
-          <Col span={24}>
-            <input type="email" placeholder="Enter your email" />
+          <Col span={16}>
+            <BasicField type="email" placeholder="Enter your email" />
           </Col>
-          <Col span={24}>
-            <input type="password" placeholder="Enter your password" />
+          <Col span={16}>
+            <BasicField type="password" placeholder="Enter your password" />
           </Col>
           <Col span={24}>
             <button>Submit</button>
