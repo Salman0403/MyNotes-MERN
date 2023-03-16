e7fcf213f3990388bc9ee645fe0cde994f19b741
Salman0403 <muhammadsalman0403@gmail.com>
2023-03-16 18:39:48 +0500

    feat: react query and axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./container/Login/Login";
 import Signup from "./container/Signup/Signup";
 import AllNotes from "./container/AllNotes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const client = new QueryClient();
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-          <Route exact path={"/signup"} component={Signup}  />
-          <Route exact path={"/allNotes"} component={AllNotes} />
-        </Switch>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path={"/signup"} component={Signup} />
+            <Route exact path={"/allNotes"} component={AllNotes} />
+          </Switch>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
